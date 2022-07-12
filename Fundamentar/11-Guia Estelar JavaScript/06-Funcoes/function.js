9b23761c5728b6bb646d722deda143d68c93eb4e
Reginaldo Sena <regsena@gmail.com>
2022-07-11 22:56:22 -0400

    update js

--- Fundamentar/11-Guia Estelar JavaScript/06-Funcoes/function.js
+++ Fundamentar/11-Guia Estelar JavaScript/06-Funcoes/function.js
@@ -2,5 +2,16 @@
 // anonymous function
 
 const sum = function(){
-    
+
 }
+
+// executar a função acima
+sum();
+
+// parâmetros (parameters) 
+const sum = function(){
+
+}
+
+// executar a função acima
+sum();
