7a10859413ff7287eedabc685a7f718f8e0e4913
Reginaldo Sena <regsena@gmail.com>
2022-07-12 22:55:22 -0400

    update md

--- Fundamentar/11-Guia Estelar JavaScript/06-Funcoes/function.js
+++ Fundamentar/11-Guia Estelar JavaScript/06-Funcoes/function.js
@@ -22,12 +22,14 @@ console.log("==========================================");
 function Person1(name){
     this.name = name;
     this.walk = function(){
-        return "andando";
+        return this.name + " está andando...";
     }
 }
 
 const reginaldo1 = new Person1("Regi");
 console.log(reginaldo1);
+console.log(reginaldo1.walk());
 
 const melissa1 = new Person1("Mel");
 console.log(melissa1);
+console.log(melissa1.walk());
