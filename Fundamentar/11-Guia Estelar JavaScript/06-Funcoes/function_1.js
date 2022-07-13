a612897d018cf54bd905b2a6812aa38a2500ca96
Reginaldo Sena <regsena@gmail.com>
2022-07-12 21:14:54 -0400

    update md

---
+++ Fundamentar/11-Guia Estelar JavaScript/06-Funcoes/function_1.js
@@ -0,0 +1,22 @@
+console.log("*********** Aula sobre functions ***********");
+
+// Criar um aplicativo de frases motivacionais
+
+// function declaration - declaração de função
+// function statement
+function createPhrases(){
+    console.log("- Estudar é muito bom");
+    console.log("- Paciência e persistência");
+    console.log("- Revisão é mãe do aprendizado");
+}
+
+// executar a função
+// rodar, chamar, invocar
+// execute, call, invoke and run
+createPhrases();
+
+console.log("Fim do programa.");
+
+
+
+
