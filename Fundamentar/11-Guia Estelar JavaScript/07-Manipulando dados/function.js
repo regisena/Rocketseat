72f405a7bec087729dc62717757b62040a46e8ad
Reginaldo Sena <regsena@gmail.com>
2022-07-13 21:21:22 -0400

    update md

--- Fundamentar/11-Guia Estelar JavaScript/07-Manipulando dados/function.js
+++ Fundamentar/11-Guia Estelar JavaScript/07-Manipulando dados/function.js
@@ -8,4 +8,5 @@ console.log(word.length + " caracteres");
 console.log("==========================");
 let num = 1234;
 console.log(num);
-console.log(num.length);
+console.log(num.length); // undefined
+console.log(String(num).length);
