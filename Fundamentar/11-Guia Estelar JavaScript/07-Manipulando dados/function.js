6e9eef6856c1c96758585b3a091a8f2ea0ce1082
Reginaldo Sena <regsena@gmail.com>
2022-07-13 22:35:47 -0400

    update md

--- Fundamentar/11-Guia Estelar JavaScript/07-Manipulando dados/function.js
+++ Fundamentar/11-Guia Estelar JavaScript/07-Manipulando dados/function.js
@@ -1,12 +1,6 @@
 // Manipulando Strings e Números
 
-// Contar quantos caracteres tem uma palavra e quantos dígitos tem um número
+// Transformar um número quebrado com 2 casas decimais e trocar ponto por vírgula
 
-let word = "Paralelepípedo";
-console.log(word);
-console.log(word.length + " caracteres");
-console.log("==========================");
-let num = 1234;
+let num = 3.1415;
 console.log(num);
-console.log(num.length); // undefined
-console.log(String(num).length);
