3334fd10f9db169ee843df563a47a722a079977b
Reginaldo Sena <regsena@gmail.com>
2022-07-13 21:05:08 -0400

    update md

---
+++ Fundamentar/11-Guia Estelar JavaScript/07-Manipulando dados/function_1.js
@@ -0,0 +1,10 @@
+/*
+    Prototype
+
+    * prototype-based language
+    * prototype chain (cadeia de protótipos)
+    * * __proto__ (propriedade ascendente)
+    * 
+*/
+
+// no console informar "alguma coisa".__proto__, vc terá acesso a diversas funções
