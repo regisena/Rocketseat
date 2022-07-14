04020f5457a34892cc9606e11e8e1448bd4f2b34
Reginaldo Sena <regsena@gmail.com>
2022-07-13 21:05:35 -0400

    update md

---
+++ Fundamentar/11-Guia Estelar JavaScript/07-Manipulando dados/function_2.js
@@ -0,0 +1,12 @@
+/*
+    Type conversion (typecasting) vs Type coersion
+
+    * Alteração de um tipo de dado para outro tipo
+
+*/
+
+// coersion
+console.log("9" + 5); // o que ocorrerá? concatenação, transformou um tipo number para tipo string, o valor 5
+
+// conversion
+console.log(Number("9") + 5);
