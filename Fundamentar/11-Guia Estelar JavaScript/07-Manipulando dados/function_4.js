f3cfefe9520e56215221f53f8f1c4d1b61ac801f
Reginaldo Sena <regsena@gmail.com>
2022-07-13 22:32:26 -0400

    update md

---
+++ Fundamentar/11-Guia Estelar JavaScript/07-Manipulando dados/function_4.js
@@ -0,0 +1,12 @@
+// Manipulando Strings e Números
+
+// Contar quantos caracteres tem uma palavra e quantos dígitos tem um número
+
+let word = "Paralelepípedo";
+console.log(word);
+console.log(word.length + " caracteres");
+console.log("==========================");
+let num = 1234;
+console.log(num);
+console.log(num.length); // undefined
+console.log(String(num).length);
