f6f76f0da77cd9beed80f088bbb4a339f0340746
Reginaldo Sena <regsena@gmail.com>
2022-07-14 22:48:21 -0400

    update md

---
+++ Fundamentar/11-Guia Estelar JavaScript/07-Manipulando dados/function_7.js
@@ -0,0 +1,7 @@
+// Manipulando Strings e Números
+
+// Verificar se o texto contém a palavra Amor, lembrando que JS é caseSensitive
+
+let phrase = "Eu quero viver o amor!";
+console.log(phrase.includes("Amor")); // false
+console.log(phrase.includes("amor")); // true
