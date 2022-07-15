136356697b2a854a02452e61541d81d2feb277f7
Reginaldo Sena <regsena@gmail.com>
2022-07-14 23:10:03 -0400

    update md

--- Fundamentar/11-Guia Estelar JavaScript/07-Manipulando dados/function.js
+++ Fundamentar/11-Guia Estelar JavaScript/07-Manipulando dados/function.js
@@ -9,3 +9,9 @@ let techs = ["html", "css", "js"];
 // remover do fim
 
 // remover do começo
+
+// pegar somente alguns elementos do array
+
+// remover 1 ou mais itens em quelquer posição do array
+
+// encontrar a posição de um elemento no array
