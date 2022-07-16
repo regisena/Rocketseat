27bebba8d6d9d37f5373f7d73d852b4d011c1489
Reginaldo Sena <regsena@gmail.com>
2022-07-15 23:10:29 -0400

    update md

--- Fundamentar/11-Guia Estelar JavaScript/07-Manipulando dados/function.js
+++ Fundamentar/11-Guia Estelar JavaScript/07-Manipulando dados/function.js
@@ -1,15 +1,18 @@
 // Manipulando Arrays
 
 let techs = ["html", "css", "js"];
-
+console.log("Array original: "+techs);
 // adicionar um item no fim
-
+techs.push("nodejs");
+console.log("Item adicionadono fim: "+techs);
 // adicionar no começo
-
+techs.unshift("sql");
+console.log("Item adicionado no início: "+techs);
 // remover do fim
-
+techs.pop();
+console.log("Remove do fim: "+techs);
 // remover do começo
-
+techs
 // pegar somente alguns elementos do array
 
 // remover 1 ou mais itens em quelquer posição do array
