99bb9cb28813e866d138fe0c6c07c8f5f7d3b861
Reginaldo Sena <regsena@gmail.com>
2022-07-16 22:25:33 -0400

    update md

---
+++ Fundamentar/11-Guia Estelar JavaScript/08-Expressoes e operadores/function_3.js
@@ -0,0 +1,16 @@
+/*
+    operadores unários
+    typeof
+    delete
+*/
+
+console.log(typeof "Reginaldo");
+
+const person = {
+    name: "Reginaldo",
+    age: 45
+}
+console.log(person);
+delete person.age;
+console.log("age deleted");
+console.log(person);
