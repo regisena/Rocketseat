db0f2d01045ecb5242d1229d4f8e6ce5d2b84deb
Reginaldo Sena <regsena@gmail.com>
2022-07-17 19:55:13 -0400

    update md

--- Fundamentar/11-Guia Estelar JavaScript/08-Expressoes e operadores/function.js
+++ Fundamentar/11-Guia Estelar JavaScript/08-Expressoes e operadores/function.js
@@ -1,15 +1,3 @@
 // Operadores de comparação
 
-// Irá comparar valores e retornar um Boolean como resposta à comparação
-
-let one = 1;
-let two = 2;
-
-// ==   igual a
-// console.log( two == 1); // false
-// console.log( one == "1"); // true
-
-// !=   diferente de
- console.log( one !=  two);  // true
- console.log( one != 1);      // false
- console.log( one != "1");    // false
+// === estritamente igual a
