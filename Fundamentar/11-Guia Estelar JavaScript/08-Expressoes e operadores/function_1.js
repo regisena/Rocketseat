ed36995c9ccb851b5daacf6db91db325de825e61
Reginaldo Sena <regsena@gmail.com>
2022-07-16 22:23:16 -0400

    update md

---
+++ Fundamentar/11-Guia Estelar JavaScript/08-Expressoes e operadores/function_1.js
@@ -0,0 +1,29 @@
+/*
+
+    Expressões e Operadores
+
+    - Expressions
+    - Operators
+        Binary
+        Unary
+        Ternary
+
+*/
+
+// expression declaration
+let number; // undefined
+console.log("=========================");
+number = 1; // initialization
+console.log("=========================");
+// binary operator
+console.log(number + 1);
+console.log("=========================");
+// unary operator
+console.log(++number);
+//or
+console.log(typeof number);
+console.log("=========================");
+// ternary operator
+console.log(true ? 'Verdadeiro' : 'Falso');
+console.log("=========================");
+
