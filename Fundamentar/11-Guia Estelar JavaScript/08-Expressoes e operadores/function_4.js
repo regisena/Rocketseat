941336e0d5ef11e4ca70f2f26078c91f53fa9db7
Reginaldo Sena <regsena@gmail.com>
2022-07-16 22:26:53 -0400

    update md

---
+++ Fundamentar/11-Guia Estelar JavaScript/08-Expressoes e operadores/function_4.js
@@ -0,0 +1,37 @@
+/*
+    Operadores Aritméticos
+
+    multiplicação
+    divisão
+    soma
+    subtração
+
+    resto da divisão
+    incremento
+    decremento
+    exponencial
+*/
+
+// multiplicação *
+console.log("multiplicação: "+3.2 * 5); // 16
+// divisão /
+console.log("divisão: "+12 / 2); // 6
+// soma +
+console.log("soma: "+34 + 67); // 101
+// subtração -
+console.log("subtração: "+`${34 - 23}`); // 11
+
+// resto da divisão %
+let remainder;
+remainder = 11 % 3;
+console.log("resto: "+remainder); // 2
+// incremento ++
+let incremento = 0; // 1
+incremento++;
+console.log("incremento: "+incremento); //1
+// decremento --
+let decremento = 0; // -1
+decremento--;
+console.log("decremento: "+decremento);
+// exponencial **
+console.log("exponencial: "+3 ** 3); // 27
