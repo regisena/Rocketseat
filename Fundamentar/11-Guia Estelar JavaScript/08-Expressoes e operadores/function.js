0c28665cb5a669dc380d2c512a4d7ceb74907b6d
Reginaldo Sena <regsena@gmail.com>
2022-07-17 20:16:57 -0400

    update md

---
+++ Fundamentar/11-Guia Estelar JavaScript/08-Expressoes e operadores/function.js
@@ -0,0 +1,23 @@
+// Operadores de atribuição (Assignment)
+let x;
+console.log(x); // undefined
+
+// assignment
+x = 1;
+console.log(x); // 1
+
+// addition assignment
+x += 2;
+console.log(x); // 3
+
+// subtraction assignment
+x -= 1;
+console.log(x); // 2
+
+// multiplication assignment
+x *= 2;
+console.log(x); // 4
+
+// division assignment
+x /= 2;
+console.log(x); // 2
