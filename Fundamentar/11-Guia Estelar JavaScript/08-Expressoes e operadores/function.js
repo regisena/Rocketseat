5054c8bf5a03aea5a467a2fd9550f173740a309e
Reginaldo Sena <regsena@gmail.com>
2022-07-18 22:32:10 -0400

    update md

--- Fundamentar/11-Guia Estelar JavaScript/08-Expressoes e operadores/function.js
+++ Fundamentar/11-Guia Estelar JavaScript/08-Expressoes e operadores/function.js
@@ -1,16 +1,3 @@
-// Operador Condicional (Ternário)
-
-/*Os operadores ternários, conhecidos como operadores de condição, como o nome sugere, são dependentes de condições e podem entregar valores diferentes com base nelas.*/
-
-//Funciona da seguinte forma;
-
-condição ? valor1 : valor2
-//Exemplo de uso:
-
-// Café da manhã top
-let pao = false
-let queijo = false
-
-const niceBreakfast = pao || queijo ? 'Café top' : 'Café ruim'
-
-console.log(niceBreakfast)
+/*
+    Type conversion (typecasting) vs Type coersion
+*/
