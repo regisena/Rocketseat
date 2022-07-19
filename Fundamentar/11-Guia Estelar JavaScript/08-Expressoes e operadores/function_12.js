6979777e61edbaeac1f02fb802eb19596cb7f306
Reginaldo Sena <regsena@gmail.com>
2022-07-18 22:33:12 -0400

    update md

---
+++ Fundamentar/11-Guia Estelar JavaScript/08-Expressoes e operadores/function_12.js
@@ -0,0 +1,12 @@
+// Operador de String (String operator)
+
+// comparison (comparação)
+console.log("a" == "b"); // false
+console.log("a" == "a"); // true
+
+// concatenation (concatenação)
+// Retorna a união de duas Strings
+let alpha = "alpha";
+console.log("a"+"a"); // aa
+console.log(alpha + "bet" + "s"); // alphabets
+console.log(alpha += "bet"); // alphabet
