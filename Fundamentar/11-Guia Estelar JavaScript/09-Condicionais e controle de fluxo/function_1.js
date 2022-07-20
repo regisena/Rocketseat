597a24668e81ad58c2f44ccb0a49dfc31f420534
Reginaldo Sena <regsena@gmail.com>
2022-07-19 22:38:44 -0400

    update md

---
+++ Fundamentar/11-Guia Estelar JavaScript/09-Condicionais e controle de fluxo/function_1.js
@@ -0,0 +1,11 @@
+// Control flow
+
+// fluxo padrão - leitura lina a linha - Top down
+console.log("a");
+console.log("b");
+
+// se o copo estiver sujo
+console.log("lavar o copo");
+
+// senão estiver sujo
+console.log("servir café");
