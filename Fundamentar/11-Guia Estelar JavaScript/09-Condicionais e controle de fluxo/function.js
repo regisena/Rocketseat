7a3bc5a710f7bf1d3590dfe085c8ed40ac36a6fe
Reginaldo Sena <regsena@gmail.com>
2022-07-19 22:43:34 -0400

    update md

--- Fundamentar/11-Guia Estelar JavaScript/09-Condicionais e controle de fluxo/function.js
+++ Fundamentar/11-Guia Estelar JavaScript/09-Condicionais e controle de fluxo/function.js
@@ -1,8 +1,15 @@
+/*
+Nesta aula veremos sobre o throw e try/catch. São parte do controle de fluxo da aplicação.
+
+Throw em inglês significa lançar, disparar, catch quer dizer pegar e try tentar.
+
+Isso significa que vamos tentar executar um bloco de código, e se der algum erro, será disparado e capturado na nossa aplicação. Suponhamos que haja uma função que dispare um erro caso não seja passado um parâmetro dessa função.
+*/
 // throw (disparar/lançar)
 
 function sayMyName(name = "") {
     if(name ===""){
-        
+
     }
 }
 // try... catch (tentar... pegar)
