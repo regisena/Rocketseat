a10462aaf0860751f38cf55ce00c8f105a51db91
Reginaldo Sena <regsena@gmail.com>
2022-07-20 22:59:23 -0400

    update md

--- Fundamentar/11-Guia Estelar JavaScript/11-Prática/function.js
+++ Fundamentar/11-Guia Estelar JavaScript/11-Prática/function.js
@@ -11,9 +11,9 @@ Crie um algoritmo que transforme as notas do sistema
 numérico para sistema de notas em caracteres tipo A B C
 
 * de 90 para cima -   A
-* entre 80 - 89   -   B
-* entre 70 - 79   -   C
-* entre 60 - 69   -   D
+* de 80 a 89   -   B
+* de 70 a 79   -   C
+* de 60 a 69   -   D
 * menor que 60    -   F
 
 
@@ -21,19 +21,19 @@ numérico para sistema de notas em caracteres tipo A B C
 
 function converteNotas(nota){
     let notaConvertida = 0;
-    if(nota < 60){
+    if(nota < 60 && nota >= 0){
         notaConvertida = "nota F";
-    }else if(nota > 60 && nota < 69){
+    }else if(nota >= 60 && nota <= 69){
         notaConvertida = "nota D";
-    }else if(nota > 70 && nota < 79){
+    }else if(nota >= 70 && nota <= 79){
         notaConvertida = "nota C";
-    }else if(nota > 80 && nota < 89){
+    }else if(nota >= 80 && nota <= 89){
         notaConvertida = "nota B";
-    }else if(nota > 90){
+    }else if(nota >= 90){
         notaConvertida = "nota A";
     }
 
     return notaConvertida;
 }
 
-console.log(converteNotas(991));
+console.log(converteNotas(60));
