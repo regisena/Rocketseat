a65636786abbeb8f50da1fb2a51dbf814beeac78
Reginaldo Sena <regsena@gmail.com>
2022-07-20 22:35:01 -0400

    update md

---
+++ Fundamentar/11-Guia Estelar JavaScript/10-Estrutura de repetição/function_2.js
@@ -0,0 +1,20 @@
+// Estrutura de repetição
+// while
+
+// aqui há um while infinito
+/*while (true) {
+    
+}*/
+
+let i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+console.log("======================");
+let i1 = 49586394505;
+while (i1 > 10) {
+    console.log(i1);
+    i1 /= 35;
+}
+
