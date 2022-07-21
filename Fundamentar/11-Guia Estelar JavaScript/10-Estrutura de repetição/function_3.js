53fd981be90b97723adfa1d30390b6c30bb9d971
Reginaldo Sena <regsena@gmail.com>
2022-07-20 22:35:26 -0400

    update md

---
+++ Fundamentar/11-Guia Estelar JavaScript/10-Estrutura de repetição/function_3.js
@@ -0,0 +1,11 @@
+// for...of
+ let me = "Reginaldo";
+ let names = ["João","Paulo","Pedro"];
+
+ for(let char of me){
+    console.log(char);
+ }
+ console.log("========================");
+ for(let name of names){
+    console.log(name);
+ }
